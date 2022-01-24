0771422aefbc144834563198f9ee27beebcf7f35
Cristalswift <98321592+Cristalswift@users.noreply.github.com>
2022-01-24 19:13:03 +0100

    Add files via upload

---
+++ Chapitre1.playground/Contents.swift
@@ -0,0 +1,28 @@
+//=================================================================================
+// Le langage Swift : Apprendre la programmation avec Méthode, Clarté et Concision
+// Les Éditions Cristalswift
+// www.cristalswift.com
+// © 2022 Igor AGBOSSOU
+// Chapitre 1. Concepts de base et le logiciel de programmation Xcode
+//==================================================================================
+import UIKit
+
+var str = "Hello, playground"
+
+// Ceci est un commentaire. Il ne sera pas exécuté !
+
+// Ceci est un commentaire.
+// Il ne sera pas exécuté !
+
+/* Ceci est aussi un commentaire.
+    Organisé sur plusieurs lignes
+    Plusieurs lignes
+    Des lignes additionnelles */
+
+/* Ceci est aussi un commentaire.
+    /* Et à l’intérieur se trouve un autre commentaire */
+
+  Organisé sur plusieurs lignes
+ */
+
+let finDeCommentaire = "C'est ici que se terminent les commentaires simples"
